637cab9737b9b0f7b4a96de78115b4c749c09af1
{TheSourceCode} <The-SourceCode@users.noreply.github.com>
2017-07-28 15:59:52 -0400

    episode_20

---
+++ Episode_20/maventest/src/main/java/me/tsccoding/maven/MavenMain.java
@@ -0,0 +1,9 @@
+package me.tsccoding.maven;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MavenMain extends JavaPlugin {
+    public void onEnable(){
+        this.getLogger().info("Maven Works");
+    }
+}
